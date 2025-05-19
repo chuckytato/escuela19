fa68a5639f7a30911fc10d538a00f3e750067c20
chuckytato <chuckytato08@gmail.com>
2025-05-18 22:15:30 -0300

    Add files via upload

---
+++ cooperadora/cooperadora.js
@@ -0,0 +1,23 @@
+ function toggleMenu() {
+      const menu = document.getElementById('menuDrawer');
+      const overlay = document.getElementById('overlay');
+      const isOpen = !menu.classList.contains('translate-x-full');
+
+      menu.classList.toggle('translate-x-full');
+      overlay.classList.toggle('hidden');
+
+      if (isOpen) {
+        document.body.classList.remove('overflow-hidden');
+      } else {
+        document.body.classList.add('overflow-hidden');
+      }
+    }
+
+    window.addEventListener('load', () => {
+      const menu = document.getElementById('menuDrawer');
+      const overlay = document.getElementById('overlay');
+
+      menu.classList.add('translate-x-full');  // menú oculto al cargar
+      overlay.classList.add('hidden');          // overlay oculto
+      document.body.classList.remove('overflow-hidden'); // scroll activo
+    });
